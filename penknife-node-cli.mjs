7fb1b35f76b1abe36778c6e2f5bbe73c1c9167d1
Rocketnia <rokitna@hotmail.com>
2022-01-09 18:20:50 -0800

    Add a `penknife-lang run` command.

--- penknife-node-cli.mjs
+++ penknife-node-cli.mjs
@@ -5,6 +5,7 @@ import $url from "url";
 
 import commander from "commander";
 import fse from "fs-extra";
+import spawn from "cross-spawn";
 
 const __dirname = $path.dirname($url.fileURLToPath(import.meta.url));
 
@@ -34,6 +35,27 @@ program.command("copy-into <dir>")
         for (const file of files) {
             fse.copy($path.join(__dirname, file), $path.join(dir, file));
         }
+    });
+
+program.command("run")
+    .allowExcessArguments(false)
+    .description("start an interactive Penknife session")
+    .action(() => {
+        spawn("npm", [
+            "run",
+            "--silent",
+            "--",
+            "rainbow",
+            "-e",
+            "(= fwarc-dir* \"lib/framewarc/\" penknife-dir* \"lib/penknife/\")",
+            "(load:+ fwarc-dir* \"loadfirst.arc\")",
+            "(load:+ penknife-dir* \"penknife.arc\")",
+            "(pkrepl)",
+            "-q"
+        ], { cwd: $path.join(__dirname, "dist/arc/"), stdio: "inherit" })
+            .on("close", code => {
+                process.exit(code);
+            });
     });
 
 await program.parseAsync();
